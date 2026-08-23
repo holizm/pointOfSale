2d60e801139ec5f3c85bed8babaabee8cfc5917c
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:21 +0330

    committed by script

--- site/parts/receipt.jsx
+++ site/parts/receipt.jsx
@@ -1,6 +1,6 @@
 export default ({ posTransaction }) => <article class='receipt'>
     <h2 class='number'>{posTransaction.number}</h2>
-    <time class='transactionDate'>{posTransaction.transactionDate}</time>
+    <time class='date'>{posTransaction.transactionDate}</time>
     <span class='total'>{posTransaction.total}</span>
     <span class='currency'>{posTransaction.currency?.title}</span>
 </article>
