c00de4290eaa94f8786a6699388fdb7d3ed7a317
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:00 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='pointOfSale'
+    name='pointOfSale'
+    title='pointOfSalePointOfSale'
+    {...props}
+/>
